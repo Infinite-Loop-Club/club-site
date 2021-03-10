4ddbda6e48ede4ff13754ae20968ebbd5b285eb2
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2021-03-10 21:01:05 +0530

    added button effects

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,14 +5,26 @@ export default function Button({ children, ...props }) {
 }
 
 const Btn = styled.button`
-  font-family: 'JetBrains Mono', monospace;
-  font-size: 1.6rem;
-  padding: 1rem 2rem;
-  background-color: #3aceb2;
-  color: white;
-  outline: none;
-  border: none;
-  text-transform: uppercase;
-  border-radius: 2rem;
-  cursor: pointer;
+  &,
+  &:link {
+    font-family: 'JetBrains Mono', monospace;
+    font-size: 1.6rem;
+    padding: 1rem 2rem;
+    background-color: #3aceb2;
+    color: #000;
+    box-shadow: 0 0.3rem 1rem rgba(0, 0, 0, 0.3);
+    outline: none;
+    border: none;
+    text-transform: uppercase;
+    border-radius: 2rem;
+    cursor: pointer;
+  }
+
+  &:hover {
+    box-shadow: 0 0.4rem 1.1rem rgba(0, 0, 0, 0.3);
+  }
+
+  &:active {
+    box-shadow: 0 0.2rem 0.6rem rgba(0, 0, 0, 0.3);
+  }
 `;
