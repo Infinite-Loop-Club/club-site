95c726117fdc0f16805da8432474c47e7d3723d3
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-03-15 19:45:55 +0530

    Footer added in about-us

--- src/About.js
+++ src/About.js
@@ -1,58 +1,61 @@
 import styled from 'styled-components';
 
-import { NavigationBar } from './components';
+import { Footer, NavigationBar } from './components';
 import Card from './components/Card';
 import man from './svg/man.png';
 import H1 from './components/H1';
 
 export default function About() {
 	return (
-		<section>
-			<NavigationBar></NavigationBar>
-			<TitleContainer>
-				<H1>About Us</H1>
-			</TitleContainer>
-			<TeamContainer>
-				<h2>Management Team :</h2>
-				<InnerContainer>
-					<Card imgSrc={man} name='man' position='Vice President' />
-					<Card imgSrc={man} name='man' position='President' />
-					<Card imgSrc={man} name='man' position='Tech Lead' />
-				</InnerContainer>
-			</TeamContainer>
-			<TeamContainer>
-				<h2>Technical Team :</h2>
-				<InnerContainer>
-					<Card imgSrc={man} name='man' position='Vice President' />
-					<Card imgSrc={man} name='man' position='President' />
-					<Card imgSrc={man} name='man' position='Tech Lead' />
-				</InnerContainer>
-			</TeamContainer>
-			<TeamContainer>
-				<h2>Staff Team :</h2>
-				<InnerContainer>
-					<Card imgSrc={man} name='man' position='Vice President' />
-					<Card imgSrc={man} name='man' position='President' />
-					<Card imgSrc={man} name='man' position='Tech Lead' />
-				</InnerContainer>
-			</TeamContainer>
-			<TeamContainer>
-				<h2>External Affairs Team :</h2>
-				<InnerContainer>
-					<Card imgSrc={man} name='man' position='Vice President' />
-					<Card imgSrc={man} name='man' position='President' />
-					<Card imgSrc={man} name='man' position='Tech Lead' />
-				</InnerContainer>
-			</TeamContainer>
-			<TeamContainer>
-				<h2>Teaching Team :</h2>
-				<InnerContainer>
-					<Card imgSrc={man} name='man' position='Vice President' />
-					<Card imgSrc={man} name='man' position='President' />
-					<Card imgSrc={man} name='man' position='Tech Lead' />
-				</InnerContainer>
-			</TeamContainer>
-		</section>
+		<>
+			<section>
+				<NavigationBar></NavigationBar>
+				<TitleContainer>
+					<H1>About Us</H1>
+				</TitleContainer>
+				<TeamContainer>
+					<h2>Management Team :</h2>
+					<InnerContainer>
+						<Card imgSrc={man} name='man' position='Vice President' />
+						<Card imgSrc={man} name='man' position='President' />
+						<Card imgSrc={man} name='man' position='Tech Lead' />
+					</InnerContainer>
+				</TeamContainer>
+				<TeamContainer>
+					<h2>Technical Team :</h2>
+					<InnerContainer>
+						<Card imgSrc={man} name='man' position='Vice President' />
+						<Card imgSrc={man} name='man' position='President' />
+						<Card imgSrc={man} name='man' position='Tech Lead' />
+					</InnerContainer>
+				</TeamContainer>
+				<TeamContainer>
+					<h2>Staff Team :</h2>
+					<InnerContainer>
+						<Card imgSrc={man} name='man' position='Vice President' />
+						<Card imgSrc={man} name='man' position='President' />
+						<Card imgSrc={man} name='man' position='Tech Lead' />
+					</InnerContainer>
+				</TeamContainer>
+				<TeamContainer>
+					<h2>External Affairs Team :</h2>
+					<InnerContainer>
+						<Card imgSrc={man} name='man' position='Vice President' />
+						<Card imgSrc={man} name='man' position='President' />
+						<Card imgSrc={man} name='man' position='Tech Lead' />
+					</InnerContainer>
+				</TeamContainer>
+				<TeamContainer>
+					<h2>Teaching Team :</h2>
+					<InnerContainer>
+						<Card imgSrc={man} name='man' position='Vice President' />
+						<Card imgSrc={man} name='man' position='President' />
+						<Card imgSrc={man} name='man' position='Tech Lead' />
+					</InnerContainer>
+				</TeamContainer>
+			</section>
+			<Footer />
+		</>
 	);
 }
 
