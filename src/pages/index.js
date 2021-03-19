d58b9d37c5f20fb6a315b29d1b5a1f19a5f75f88
Santhosh <santhoshvelr@yahoo.com>
2021-03-19 16:25:15 +0530

    added posts page (#21)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
 export { default as Home } from './home';
 export { default as About } from './about';
 export { default as Register } from './register';
+export { default as Post } from './post/Post';
+export { default as IndividualPost } from './post/IndividualPost';
