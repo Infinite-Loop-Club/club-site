a2f867d0d61bd2508bb38509d1713d4bf98587f1
sank2000 <santhoshvelr@yahoo.com>
2021-03-22 21:56:07 +0530

    Empty container added

--- src/pages/post/index.js
+++ src/pages/post/index.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
 import { Container, Heading, Footer, LoaderContainer } from 'components';
 import Loader from 'components/Loader';
 import Card from './Card';
+import { logoColored } from 'images';
 
 export default function Post() {
 	const [load, setLoad] = useState(true);
@@ -32,6 +34,12 @@ export default function Post() {
 						<Loader />
 					</LoaderContainer>
 				)}
+				{posts.length === 0 && (
+					<Empty>
+						<img className='logo' src={logoColored} alt='logo'></img>
+						<h2>Currently no post available</h2>
+					</Empty>
+				)}
 				{posts.map((val, ind) => {
 					return <Card key={ind} value={val} ind={ind} />;
 				})}
@@ -40,3 +48,19 @@ export default function Post() {
 		</>
 	);
 }
+
+const Empty = styled.div`
+	min-height: 80vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	h2 {
+		text-align: center;
+		text-transform: uppercase;
+	}
+
+	img {
+		width: 8rem;
+	}
+`;
