09e093fa3b7b7713ef55179d0316a77d345b18a6
Santhosh <santhoshvelr@yahoo.com>
2021-03-30 15:51:12 +0530

    post view updated (#53)
    * post view updated
    
    * file saver added to dependency
    
    * a tag style fixed
    
    * a tag bug  fix 2
    
    * font updated

--- src/pages/post/IndividualPost.js
+++ src/pages/post/IndividualPost.js
@@ -1,22 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useHistory, useLocation } from 'react-router';
-import styled from 'styled-components';
-import { format } from 'date-fns';
+import Content from './Content';
 
-import { Container, Heading } from 'components';
-import { link, logoColored } from 'images';
-import Share from './Share';
-
-export default function Card({ ind }) {
+export default function IndividualPost() {
 	const { state } = useLocation();
 	const history = useHistory();
-	const [showModal, setShowModal] = useState(true);
-
-	useEffect(() => {
-		if (navigator.share) {
-			setShowModal(false);
-		}
-	}, []);
 
 	useEffect(() => {
 		if (state === undefined || state === null) {
@@ -24,120 +12,5 @@ export default function Card({ ind }) {
 		}
 	}, [state, history]);
 
-	const download = e => {
-		fetch(e.target.href, {
-			method: 'GET',
-			headers: {}
-		})
-			.then(response => {
-				response.arrayBuffer().then(function (buffer) {
-					const url = window.URL.createObjectURL(new Blob([buffer]));
-					const link = document.createElement('a');
-					link.href = url;
-					link.setAttribute('download', 'image.png'); //or any other extension
-					document.body.appendChild(link);
-					link.click();
-					document.body.removeChild(link);
-				});
-			})
-			.catch(err => {
-				console.log(err);
-			});
-	};
-
-	return (
-		<ContainerCustom key={ind}>
-			<img className='logo' src={logoColored} alt='logo'></img>
-			<Top>
-				<Left>
-					<Heading style={{ textAlign: 'left' }} gradient>
-						{state.title}&nbsp;
-					</Heading>
-					<Details>
-						<p>
-							Posted on: {format(new Date(state.createdAt), 'h:m,do LLLL')} by @{state.author}
-						</p>
-					</Details>
-				</Left>
-			</Top>
-			<Center>{state.content}</Center>
-			<Bottom showModal={showModal}>
-				{state.attachment && (
-					<Button href={state.attachment} download onClick={e => download(e)}>
-						<img src={link} alt={link}></img>
-						Download Attachment
-					</Button>
-				)}
-				<Share value={state} />
-			</Bottom>
-		</ContainerCustom>
-	);
+	return <Content data={state} />;
 }
-
-const ContainerCustom = styled(Container)`
-	margin-top: 10em;
-	position: relative;
-
-	.logo {
-		width: 8rem;
-		position: absolute;
-		top: -8em;
-
-		@media only screen and (max-width: 600px) {
-			width: 7rem;
-		}
-	}
-`;
-
-const Top = styled.div`
-	border-bottom: 2px solid #c2c2c2;
-	padding-bottom: 1em;
-`;
-
-const Left = styled.div`
-	text-align: left;
-`;
-
-const Details = styled.div`
-	p {
-		font-size: 1.2rem;
-	}
-`;
-
-const Center = styled.div`
-	font-size: 1.5rem;
-	padding: 2em 0;
-`;
-const Bottom = styled.div`
-	margin-top: 1em;
-	display: flex;
-	gap: 2em;
-	flex-direction: ${p => (p.showModal ? 'column' : 'row')};
-	align-items: ${p => (p.showModal ? 'flex-start' : 'center')};
-	img {
-		display: inline-block;
-		width: 3rem;
-		cursor: pointer;
-	}
-`;
-const Button = styled.a`
-	text-decoration: none;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	padding: 1em 2em;
-	border-radius: 2em;
-	cursor: pointer;
-	background-color: ${p => `${p.theme.primary}20`};
-	border: ${p => `2px solid ${p.theme.primary}`};
-	color: ${p => p.theme.black};
-	font-weight: 600;
-	font-size: 1.2rem;
-	font-family: inherit;
-
-	img {
-		display: inline-block;
-		width: 2rem;
-		margin-right: 1em;
-	}
-`;
