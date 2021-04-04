573e124a4566c64a554a0d44bb13389bfdfdf7d3
PranavGPR <pranav.gnanasekar23@gmail.com>
2021-04-04 12:13:50 +0530

    Added voting page

--- src/pages/voting/index.js
+++ src/pages/voting/index.js
@@ -1,5 +1,20 @@
-import { Container } from 'components';
+import styled from 'styled-components';
+import { Footer, Heading } from 'components';
+import TeamDetails from 'pages/about/TeamDetails';
+import details from './nominees';
 
 export default function Voting() {
-	return <Container>Hello</Container>;
+	return (
+		<VotingContainer>
+			<Heading gradient>Choose your Candidate!</Heading>
+			{details.map(v => {
+				return <TeamDetails title={v.title} data={v.data} />;
+			})}
+			<Footer />
+		</VotingContainer>
+	);
 }
+
+const VotingContainer = styled.div`
+	margin: 8rem auto auto;
+`;
---
+++ src/pages/voting/nominees.js
@@ -0,0 +1,86 @@
+import { man1 } from 'images';
+
+const data = [
+	{
+		title: 'President',
+		data: [
+			{
+				name: 'Member1',
+				profile: man1,
+				tag: 'call it as things pa',
+				year: 'IV CSE'
+			}
+		]
+	},
+	{
+		title: 'Vice President',
+		data: [
+			{
+				name: 'Member1',
+				profile: man1,
+				tag: 'call it as things pa',
+				year: 'III CSE'
+			},
+			{
+				name: 'Member2',
+				profile: man1,
+				tag: 'call it as things pa',
+				year: 'III CSE'
+			},
+			{
+				name: 'Member3',
+				profile: man1,
+				tag: 'call it as things pa',
+				year: 'III IT'
+			}
+		]
+	},
+	{
+		title: 'Secretary',
+		data: [
+			{
+				name: 'Member1',
+				profile: man1,
+				tag: 'call it as things pa',
+				year: 'II CSE'
+			},
+			{
+				name: 'Member2',
+				profile: man1,
+				tag: 'call it as things pa',
+				year: 'II IT'
+			}
+		]
+	},
+	{
+		title: 'Youth Representative',
+		data: [
+			{
+				name: 'Member1',
+				profile: man1,
+				tag: 'call it as things pa',
+				year: 'I CSE'
+			},
+			{
+				name: 'Member2',
+				profile: man1,
+				tag: 'call it as things pa',
+				year: 'I IT'
+			},
+			{
+				name: 'Member3',
+				profile: man1,
+				tag: 'call it as things pa',
+				year: 'I IT'
+			},
+			{
+				name: 'Member4',
+				profile: man1,
+				tag: 'call it as things pa',
+				year: 'I CSE'
+			}
+		]
+	}
+];
+
+export default data;
